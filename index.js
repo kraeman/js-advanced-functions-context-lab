b518c157cffbb2ce59e313674f169df42f33ae5c
Krae <kraewind@gmail.com>
2021-05-19 14:55:45 -0400

    Done.

--- index.js
+++ index.js
@@ -9,6 +9,69 @@
  for you to use if you need it!
  */
 
+ const createEmployeeRecord = (array) => {
+    const person = {firstName: array[0], familyName: array[1], title: array[2], payPerHour: array[3], timeInEvents: [], timeOutEvents: []}
+    return person
+}
+
+const createEmployeeRecords = (arrayOfArrays) => {
+    const arrayOfObjects = []
+    arrayOfArrays.forEach(array => arrayOfObjects.push(createEmployeeRecord(array)))
+    return arrayOfObjects
+}
+
+const createTimeInEvent = (dateStamp) => {
+    const arrayOfTime = dateStamp.split("")
+    const date = arrayOfTime.slice(0, 10)
+    const m = date.join("")
+    const thing = arrayOfTime.slice(11, 15)
+    const thingy = parseInt(thing.join(""), 10)
+    // console.log(thingy)
+    const timeInObject = {type: "TimeIn", hour: thingy, date: m}
+    this.timeInEvents.push(timeInObject)
+    return this
+}
+
+const createTimeOutEvent = (dateStamp) => {
+    const arrayOfTime = dateStamp.split("")
+    const date = arrayOfTime.slice(0, 10)
+    const m = date.join("")
+    const thing = arrayOfTime.slice(11, 15)
+    const thingy = parseInt(thing.join(""), 10)
+    // console.log(thingy)
+    const timeOutObject = {type: "TimeOut", hour: thingy, date: m}
+    this.timeOutEvents.push(timeOutObject)
+    return this
+}
+
+
+const hoursWorkedOnDate = (dateStamp) => {
+    const toe = this.timeOutEvents
+    const tie = this.timeInEvents
+    const o = toe.find(e => e.date == dateStamp)
+    const i = tie.find(e => e.date == dateStamp)
+    const hw = o.hour - i.hour
+    return hw / 100
+}
+const wagesEarnedOnDate = (date) => {
+    const hours = hoursWorkedOnDate(this, date)
+    const money = hours * this.payPerHour
+    return money
+}
+
+
+
+const calculatePayroll = (arrayOfEmployees) => {
+    let payroll = 0
+    arrayOfEmployees.forEach(e => payroll += allWagesFor(e))
+    return payroll
+}
+
+const findEmployeeByFirstName = (srcArray, firstName) => {
+    const match = srcArray.find(employee => employee.firstName == firstName)
+    return match
+}
+
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
